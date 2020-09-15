df3d64f8c415b4fa3b575557da9779f4fcc0685e
Marnes <marnes.swanepoel@gmail.com>
2020-09-15 22:55:24 +1000

    Added Variables class for demoing types and type keyword

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#This class will demonstartae the basics of pythons syntax for variavles and
+#types.
+
+name = 'Bob' #String '' or "" will do, not both
+name = 'sadas'
+name = 1234 # int
+name = False # boolean. Note booleans must start with a capitil e.g True/False
+name = 7.32 # floats
+
+#Python allows you to change the types stored in a variable
+
+name = None # This is similar to Null like in Java, C#, etc.
+
+# Using type will return variable type
+print(type(name))
